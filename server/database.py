7c7d204b95975ad08a115bf414b24933b90a7528
AnkithGoje <ankithgoje25@gmail.com>
2025-12-29 11:48:08 +0530

    Add PostgreSQL support for Render deployment

--- server/database.py
+++ server/database.py
@@ -2,10 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+import os
+
+# Get DB URL from env or fallback to local sqlite
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+connect_args = {}
+
+if SQLALCHEMY_DATABASE_URL:
+    # Render provides 'postgres://' but SQLAlchemy needs 'postgresql://'
+    if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+else:
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+    connect_args = {"check_same_thread": False}
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
